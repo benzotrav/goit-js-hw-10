78c6d0f08d601eee83b0eae0051b371ad914f875
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-05-24 22:22:37 +0300

    bug fixed in function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
 import debounce from 'lodash.debounce';  
 import Notiflix from 'notiflix'; 
 import { fetchCountries } from './js/task1';  
